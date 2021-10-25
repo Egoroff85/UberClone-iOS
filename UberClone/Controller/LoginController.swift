c9a20b5adb8d3124fc05cc3607aa72e2e70d765b
Andrey Egorov <andreyegorov1985@gmail.com>
2021-10-26 01:08:27 +0300

    login controller added

--- UberClone/Controller/LoginController.swift
+++ UberClone/Controller/LoginController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginController.swift
 //  UberClone
 //
 //  Created by WiCKed on 26.10.2021.
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        view.backgroundColor = .red
     }
 
-
 }
-
