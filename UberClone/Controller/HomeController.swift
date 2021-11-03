ceb93b4dfd25a78ab60cfd931d9e6cee8cbc0421
Andrey Egorov <andreyegorov1985@gmail.com>
2021-11-03 22:56:28 +0300

    modified map zooming

--- UberClone/Controller/HomeController.swift
+++ UberClone/Controller/HomeController.swift
@@ -214,8 +214,14 @@ class HomeController: UIViewController {
         }, completion: completion)
     }
     
-    func animateRideActionView(shouldShow: Bool) {
+    func animateRideActionView(shouldShow: Bool, destination: MKPlacemark? = nil) {
         let yOrigin = shouldShow ? self.view.frame.height - self.rideActionViewHeight : self.view.frame.height
+        
+        if shouldShow {
+            guard let destination = destination else { return }
+            rideActionView.destination = destination
+        }
+        
         UIView.animate(withDuration: 0.3) {
             self.rideActionView.frame.origin.y = yOrigin
         }
@@ -374,8 +380,8 @@ extension HomeController: UITableViewDelegate, UITableViewDataSource {
             self.mapView.addAnnotation(annotation)
             self.mapView.selectAnnotation(annotation, animated: true)
             let annotations = self.mapView.annotations.filter({ !$0.isKind(of: DriverAnnotation.self)})
-            self.mapView.showAnnotations(annotations, animated: true)
-            self.animateRideActionView(shouldShow: true)
+            self.mapView.zoomToFit(annotations: annotations)
+            self.animateRideActionView(shouldShow: true, destination: selectedPlacemark)
         }
     }
 }
