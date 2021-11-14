5bbdf3a49f3b3234b3af12bc5124e243a311ad20
Andrey Egorov <andreyegorov1985@gmail.com>
2021-11-14 03:47:53 +0300

    added settings, added favorite locations

--- UberClone/View/UserInfoHeader.swift
+++ UberClone/View/UserInfoHeader.swift
@@ -1,14 +1,14 @@
 //
-//  MenuHeader.swift
+//  UserInfoHeader.swift
 //  UberClone
 //
-//  Created by WiCKed on 12.11.2021.
+//  Created by WiCKed on 14.11.2021.
 //
 
 import UIKit
 
-class MenuHeader: UIView {
-
+class UserInfoHeader: UIView {
+    
     // MARK: - Properties
     
     private let user: User
@@ -23,7 +23,6 @@ class MenuHeader: UIView {
         let label = UILabel()
         label.font = UIFont.systemFont(ofSize: 16)
         label.text = user.fullname
-        label.textColor = .white
         return label
     }()
     
@@ -35,16 +34,16 @@ class MenuHeader: UIView {
         return label
     }()
     
-    
     // MARK: - Lifecycle
     
     init(user: User, frame: CGRect) {
         self.user = user
         super.init(frame: frame)
-        backgroundColor = .backgroundColor
+        backgroundColor = .white
         
         addSubview(profileImageView)
-        profileImageView.anchor(top: topAnchor, left: leftAnchor, paddingTop: 4, paddingLeft: 12, width: 64, height: 64)
+        profileImageView.centerY(inView: self, leftAnchor: leftAnchor, paddingLeft: 16)
+        profileImageView.setDimensions(height: 64, width: 64)
         profileImageView.layer.cornerRadius = 64 / 2
         
         let stack = UIStackView(arrangedSubviews: [fullnameLabel, emailLabel])
@@ -58,6 +57,4 @@ class MenuHeader: UIView {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    // MARK: - Selectors
 }
